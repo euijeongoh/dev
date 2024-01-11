d57a38733048885f5f41e38885dc6b8e63ddbfad
JAVA <142917202+euijeongoh@users.noreply.github.com>
2024-01-11 15:31:09 +0900

    Update MypageMain.jsx

--- dalmagi_final/Dalmagi/dalmagiRepo/src/mypage/MypageMain.jsx
+++ dalmagi_final/Dalmagi/dalmagiRepo/src/mypage/MypageMain.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import MemberJoin from './MemberJoin';
+import { Route, Routes } from 'react-router-dom';
+import ErrorPageNotFound from '../error/ErrorPageNotFound';
+import MypageJoin from './MypageJoin';
+import MypageLogin from './MypageLogin';
+import MemberEdit from './MemberEdit';
 
 const MypageMain = () => {
     return (
             <Routes>
-                <Route path='/join' element={<MemberJoin />}/>
-                <Route path='/edit' element={<MemberEdit />}/>
+                <Route path='join' element={<MypageJoin />}/>
+                <Route path='edit' element={<MemberEdit />}/>
+                <Route path='login' element={<MypageLogin />}/>
                 <Route path='*' element={<ErrorPageNotFound />}/>
             </Routes>
     );
