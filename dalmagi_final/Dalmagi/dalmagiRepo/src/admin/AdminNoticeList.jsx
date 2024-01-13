62ffe4d7ff1a0140e9686dc12c0b662f19898d1e
JAVA <142917202+euijeongoh@users.noreply.github.com>
2024-01-13 21:58:24 +0900

    Update and rename AdminNoticeList.jsx to AdminNoticeList.jsx

--- dalmagi_final/Dalmagi/dalmagiRepo/src/admin/AdminNoticeList.jsx
+++ dalmagi_final/Dalmagi/dalmagiRepo/src/admin/AdminNoticeList.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledNoticeListDiv = styled.div`
