391d61b230c40e8e562f6fe50ba77451b4cf6c2a
JAVA <142917202+euijeongoh@users.noreply.github.com>
2024-01-14 17:39:49 +0900

    Update AdminMain.jsx

--- dalmagi_final/Dalmagi/dalmagiRepo/src/admin/AdminMain.jsx
+++ dalmagi_final/Dalmagi/dalmagiRepo/src/admin/AdminMain.jsx
@@ -3,15 +3,21 @@ import SearchAdminDelete from '../search/SearchAdminDelete';
 import AdminMyPage from './AdminMyPage';
 import { Route, Routes } from 'react-router-dom';
 import SearchAdminEdit from '../search/SearchAdminEdit';
-import AdminLogin from './AdminLogin';
+import AdminNoticeList from './AdminNoticeList';
+import AdminNoticeWrite from './AdminNoticeWrite';
+import AdminNoticeEdit from './AdminNoticeEdit';
+import AdminNoticeDetail from './AdminNoticeDetail';
 
 const AdminMain = () => {
     return (
         <Routes>
             <Route path='/mypage/*' element={<AdminMyPage />}></Route>
-            <Route path='/login/*' element={<AdminLogin />}></Route>
             <Route path='/edit/*' element={<SearchAdminEdit />}></Route>
             <Route path='/delete/*' element={<SearchAdminDelete />}></Route>
+            <Route path='/notice/list/*' element={<AdminNoticeList />}></Route>
+            <Route path='/notice/write/*' element={<AdminNoticeWrite />}></Route>
+            <Route path='/notice/edit/*' element={<AdminNoticeEdit />}></Route>
+            <Route path='/notice/detail/*' element={<AdminNoticeDetail />}></Route>
         </Routes>
     );
 };
