50dede65ae14443897e6ffa47feb4472d261865b
JAVA <142917202+euijeongoh@users.noreply.github.com>
2024-01-16 10:07:29 +0900

    Update NoticeMain.jsx

--- dalmagi_final/Dalmagi/dalmagiRepo/src/notice/NoticeMain.jsx
+++ dalmagi_final/Dalmagi/dalmagiRepo/src/notice/NoticeMain.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import AdminNoticeList from './AdminNoticeList';
-import AdminNoticeDetail from './AdminNoticeDetail';
-import AdminNoticeWrite from './AdminNoticeWrite';
-import AdminNoticeEdit from './AdminNoticeEdit';
+import NoticeList from './NoticeList';
+import ErrorPageNotFound from '../error/ErrorPageNotFound';
+import AdminNoticeWrite from '../admin/AdminNoticeWrite';
+import AdminNoticeDetail from '../admin/AdminNoticeDetail';
 
 
 const NoticeMain = () => {
     return (
         <Routes>
-            <Route path='list' element={<AdminNoticeList />}/>
+            <Route path='list' element={<NoticeList />}/>
             <Route path='write' element={<AdminNoticeWrite />}/>
             <Route path='detail' element={<AdminNoticeDetail />}/>
-            <Route path='edit' element={<AdminNoticeEdit />}/>
+            <Route path='*' element={<ErrorPageNotFound />}/>
         </Routes>
     );
 };
